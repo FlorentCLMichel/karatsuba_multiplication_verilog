5f71cc764fdf5c551ca249773e1cb6781a6aa473
Florent C. L. M. Michel <56166507+FlorentCLMichel@users.noreply.github.com>
2022-08-28 22:35:12 +0100

    first commit

--- obj_dir/Vaddition__ALL.d
+++
@@ -1,8 +0,0 @@
-Vaddition__ALL.o: Vaddition__ALL.cpp Vaddition.cpp Vaddition.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h Vaddition__Syms.h \
- Vaddition___024root.h Vaddition___024root__DepSet_h954c93d5__0.cpp \
- Vaddition___024root__Slow.cpp \
- Vaddition___024root__DepSet_h954c93d5__0__Slow.cpp Vaddition__Syms.cpp
--- obj_dir/addition.d
+++
@@ -1,5 +0,0 @@
-addition.o: ../tests/addition.cpp Vaddition.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
